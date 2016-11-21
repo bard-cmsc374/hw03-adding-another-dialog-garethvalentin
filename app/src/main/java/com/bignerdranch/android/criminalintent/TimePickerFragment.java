e399e8b451d5f0562914d76efbf4d428f2f191c6
GV <patiencebro@gmail.com>
2016-11-21 17:50:21 -0500

    first commit

--- app/src/main/java/com/bignerdranch/android/criminalintent/TimePickerFragment.java
+++ app/src/main/java/com/bignerdranch/android/criminalintent/TimePickerFragment.java
@@ -11,6 +11,7 @@ import android.support.v7.app.AlertDialog;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.DatePicker;
+import android.widget.TimePicker;
 
 import java.util.Calendar;
 import java.util.Date;
@@ -19,7 +20,7 @@ import java.util.GregorianCalendar;
 /**
  * Created by cs374 on 11/15/16.
  */
-public class DatePickerFragment extends DialogFragment {
+public class TimePickerFragment extends DialogFragment {
 
     public static final String EXTRA_DATE =
             "com.bignerdranch.android.criminalintent.date";
@@ -27,13 +28,14 @@ public class DatePickerFragment extends DialogFragment {
     private static final String ARG_DATE = "date";
 
     private DatePicker mDatePicker;
+    private TimePicker mTimePicker;
 
-    public static DatePickerFragment newInstance(Date date) {
+    public static TimePickerFragment newInstance(Date date) {
         Bundle args = new Bundle();
         args.putSerializable(ARG_DATE, date);
 
 
-        DatePickerFragment fragment = new DatePickerFragment();
+        TimePickerFragment fragment = new TimePickerFragment();
         fragment.setArguments(args);
         return fragment;
     }
@@ -54,6 +56,8 @@ public class DatePickerFragment extends DialogFragment {
         mDatePicker = (DatePicker) v.findViewById(R.id.dialog_date_date_picker);
         mDatePicker.init(year, month, day, null);
 
+        mTimePicker = (TimePicker) v.findViewById(R.id.dialog_date_time_picker);
+
         return new AlertDialog.Builder(getActivity())
                 .setView(v)
                 .setTitle(R.string.date_picker_title)
@@ -64,7 +68,9 @@ public class DatePickerFragment extends DialogFragment {
                                 int year = mDatePicker.getYear();
                                 int month = mDatePicker.getMonth();
                                 int day = mDatePicker.getDayOfMonth();
-                                Date date = new GregorianCalendar(year, month, day).getTime();
+                                int hour = mTimePicker.getHour();
+                                int minute = mTimePicker.getMinute();
+                                Date date = new GregorianCalendar(year, month, day, hour, minute).getTime();
                                 sendResult(Activity.RESULT_OK, date);
                             }
                         })
